5d4678dc6b6b79fda9ba8406399607c2f4ceeb29
mozzart <Brunocorona@live.com>
2022-07-01 14:02:32 -0300

    feature: run build and deploy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/home";
 import Success from "./pages/success/success";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" exact={true} component={App} />
+    <Route path="/" element={<Home />} />
       <Route path="/Home" element={<Home />} />
       <Route path="/success" element={<Success />} />
     </Routes>
